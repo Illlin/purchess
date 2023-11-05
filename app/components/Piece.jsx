8949f3a78790c298a41112a75107e3676bbe3435
Illlin <IllinTori@gmail.com>
2023-11-05 10:51:57 +0000

    Added pieces

--- app/components/Piece.jsx
+++ app/components/Piece.jsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 
 const Piece = ({ code, colour, onClick, style  }) => {
-    let image_url = "/pieces/" + colour.toLowerCase() + "/" + code.toLowerCase() + ".png";
+    let image_url = "/pieces/" + code.toLowerCase() + ".png";
     
 
     return (
         <button onClick={onClick} style={{ position: 'absolute', ...style }}>
-            <Image src={image_url} width={100} height={100} />
+            <Image className={colour.toLowerCase() === 'black' ? 'invert' : ''} src={image_url} width={100} height={100} />
         </button>
     );
 };
