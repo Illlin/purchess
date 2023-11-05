8f28840a8b643ea115d37cf9208eb46185f07ffe
Illlin <IllinTori@gmail.com>
2023-11-05 04:46:59 +0000

    Proxy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+
+module.exports = () => {
+    const rewrites = () => {
+        return [
+            {
+              source: '/api/:path*',
+              destination: 'http://localhost:5000/api/:path*',
+            },
+          ];
+    };
+    return {
+      rewrites,
+    };
+  };
